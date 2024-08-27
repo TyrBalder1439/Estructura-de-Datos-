4444033d09b4a400bd0e03cdd6fab2c6194a8b8b
felipe herman <felipe159cd@outlook.com>
2024-08-27 11:03:15 -0600

    actualizacion

--- Tarea_3/Nodo/src/main/java/com/mycompany/nodo/NodosMain.java
+++ Tarea_3/Nodo/src/main/java/com/mycompany/nodo/NodosMain.java
@@ -13,8 +13,7 @@ public class NodosMain {
         System.out.print("null|\n");
         System.out.println("cambiando 300 por 333");
 
-        head2.getSiguiente().getSiguiente().setDato(333);
-        
+        head2.getSiguiente().getSiguiente().setDato(333);        
         Nodo<Integer> aux = head2;
         System.out.print("|");
         while (aux != null){
@@ -22,32 +21,24 @@ public class NodosMain {
             aux = aux.getSiguiente();
         }
         System.out.print("null|\n");
-        System.out.println("integrando 700 al final del codigo");
-        
+        System.out.println("integrando 700 al final");        
         head2.getSiguiente().getSiguiente().getSiguiente().getSiguiente().setSiguiente(new Nodo<>(700));
-        
         Nodo<Integer> aux3 = head2;
         System.out.print("|");
         while (aux3 != null){
             System.out.print(aux3.getDato() + "| -> |");
             aux3 = aux3.getSiguiente();
         }
-        System.out.print("null|\n");
+        System.out.print("null|\n");      
+        System.out.println("agregando nuevo nodo al inicio");
         
-        System.out.println("agregando nuevo nodo");
-        
-        head2.setSiguiente(new Nodo<>(50,head2.getSiguiente().getSiguiente().getSiguiente().getSiguiente().getSiguiente()));
-        
+        head2 = new Nodo<>(50, head2);
         Nodo<Integer> au = head2;
         System.out.print("|");
-        while (au != null){
+        while (au != null) {
             System.out.print(au.getDato() + "| -> |");
             au = au.getSiguiente();
         }
         System.out.print("null|\n");
-        
-
-       
-
     }
 }
