e70aa1d59df40986641c5c3ca526d047287b95e0
felipe herman <felipe159cd@outlook.com>
2024-09-03 12:01:56 -0600

    agregando lo que falto

--- Tarea_4/LIstaLigada/src/Main.java
+++ Tarea_4/LIstaLigada/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
             ListaLigada<SmartPhone> lista = new ListaLigada<>();
 
             // Agregar 5 SmartPhone
-            System.out.println("Marca  Modelo   Precio");
+            lista.agregarAlFinal(new SmartPhone("Samsung", "", 999.99));
             lista.agregarAlFinal(new SmartPhone("Apple", "iPhone 12", 999.99));
             lista.agregarAlInicio(new SmartPhone("sansung", "iPhone 12", 999.99));
             SmartPhone Smart1 = new SmartPhone("Opoo", "pixel RTX 12", 9.299);
