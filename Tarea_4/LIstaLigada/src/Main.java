64095ed8dc42346fec1b3765f30cdeebc64d18b3
felipe herman <felipe159cd@outlook.com>
2024-09-03 19:24:52 -0600

    listo

--- Tarea_4/LIstaLigada/src/Main.java
+++ Tarea_4/LIstaLigada/src/Main.java
@@ -1,21 +1,29 @@
 public class Main {
         public static void main(String[] args) {
-            // Crear lista ligada de SmartPhone
             ListaLigada<SmartPhone> lista = new ListaLigada<>();
-
-            // Agregar 5 SmartPhone
-            lista.agregarAlFinal(new SmartPhone("Samsung", "Galaxi S23 plus", 16.001));
-            lista.agregarAlFinal(new SmartPhone("Apple", "iPhone 15 pro", 24.001));
-            lista.agregarAlFinal(new SmartPhone("Huawei", "p70 pro", 22.001));
-            lista.agregarAlFinal(new SmartPhone("Google", "Pixel 7 Pro ", 14.001));
-            lista.agregarAlFinal(new SmartPhone("Redmi", "Poco", 5.697));
             System.out.println("Contenido de la lista:");
+            lista.agregarAlFinal(new SmartPhone("Samsung", "Galaxy S23 plus", 16000));
+            lista.agregarAlFinal(new SmartPhone("Apple", "iPhone 15 pro", 24000));
+            lista.agregarAlFinal(new SmartPhone("Huawei", "p70 pro", 22000));
+            lista.agregarAlFinal(new SmartPhone("Google", "Pixel 7 Pro ", 14000));
+            lista.agregarAlFinal(new SmartPhone("Redmi", "Poco", 5697));
             lista.transversal();
+
+            System.out.println("\neliminar posición 2:");
             lista.eliminar(2);
-            System.out.println("\nContenido después de eliminar el elemento en la posición 2:");
             lista.transversal();
-            lista.actualizar(new SmartPhone("Redmi", "Poco", 5.697), new SmartPhone("Samsung", "Galaxy S22", 899.99));
-            System.out.println("\nContenido después de actualizar el segundo elemento:");
+
+            System.out.println("\nactualizar el segundo elemento:");
+            lista.actualizar(new SmartPhone("Apple", "iPhone 15 pro", 24000),  new SmartPhone("Nokia", "Experia x34", 9500));
+            lista.transversal();
+
+            System.out.println("\nagregar elementos al inicio y al final:");
+            lista.agregarAlInicio(new SmartPhone("Huawei", "P70 Ultra", 26500));
+            lista.agregarAlFinal(new SmartPhone("Sony", "Xperia plus", 11700));
+            lista.transversal();
+
+            System.out.println("\neliminar el primer elemento:");
+            lista.eliminarElPrimero();
             lista.transversal();
 
 
